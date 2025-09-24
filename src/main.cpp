25ea5f3d15410d712b14682b977feb337672083f
NeKon69 <nobodqwe@gmail.com>
2025-09-24 19:25:46 +0300

    made starting and ending coordinates generate, some other minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,40 @@
 #include <vector>
 #include "FastNoiseLite.h"
 
-
 #define MAX_CURRENT UINT32_MAX
 
-inline constexpr float THRESHOLD = 0.3f;
-inline constexpr uint32_t WALL = MAX_CURRENT - 2;
-inline constexpr uint32_t TARGET = MAX_CURRENT - 1;
-inline constexpr uint32_t EMPTY = 0;
+using type = uint32_t;
+using position = std::pair<type, type>;
 
-void find_shortest_path(std::vector<std::vector<int>> &mat) {}
+inline constexpr float THRESHOLD = 0.4f;
+inline constexpr type WALL = MAX_CURRENT - 2;
+inline constexpr type TARGET = MAX_CURRENT - 1;
+inline constexpr type EMPTY = 0;
+inline constexpr type SIZE = 100;
+inline constexpr type SEED = 123;
 
-void prepare_matrix(std::vector<std::vector<unsigned int>> &mat, const FastNoiseLite &noise) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+
+void find_shortest_path(std::vector<std::vector<type>> &mat, position start, position end) {
+    position curr = start;
+    while (curr != end) {
+    }
+}
+
+std::pair<position, position> prepare_matrix(std::vector<std::vector<type>> &mat, const FastNoiseLite &noise) {
+    std::mt19937 gen(SEED);
     std::uniform_real_distribution<float> dist(-0.5, 0.5);
+    std::uniform_int_distribution<type> target_dist(0, SIZE - 1);
+    const type row1 = target_dist(gen);
+    const type col1 = target_dist(gen);
+    mat[row1][col1] = TARGET;
+    const type row2 = target_dist(gen);
+    const type col2 = target_dist(gen);
+    mat[row2][col2] = TARGET;
+
     for (auto row = 0; row < mat.size(); row++) {
         for (auto col = 0; col < mat[row].size(); col++) {
+            if (mat[row][col] == TARGET)
+                continue;
             auto nois_val = (noise.GetNoise(float(row), float(col)) + 1) / 2;
             if (nois_val + dist(gen) > THRESHOLD)
                 mat[row][col] = WALL;
@@ -29,7 +47,7 @@ void prepare_matrix(std::vector<std::vector<unsigned int>> &mat, const FastNoise
         }
     }
 }
-void print_matrix(const std::vector<std::vector<unsigned int>> &mat) {
+void prtype_matrix(const std::vector<std::vector<type>> &mat) {
     for (const auto &row: mat) {
         for (const auto &cell: row) {
             if (cell == WALL) {
@@ -47,18 +65,19 @@ void print_matrix(const std::vector<std::vector<unsigned int>> &mat) {
 }
 
 int main() {
-    // We use (MAX_CURRENT - 1) as starting/ending positions, and MAX_CURRENT - 2 as walls
-    std::vector mat(100, std::vector<unsigned int>(100));
+    // We use (MAX_CURRENT - 1) as starting/ending positions, and (MAX_CURRENT - 2) as walls
+    std::vector mat(SIZE, std::vector<type>(SIZE));
     FastNoiseLite noise;
-    noise.SetSeed(123);
+    noise.SetSeed(SEED);
     noise.SetFrequency(0.5f);
     noise.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
     noise.SetCellularReturnType(FastNoiseLite::CellularReturnType_Distance2Sub);
     noise.SetCellularDistanceFunction(FastNoiseLite::CellularDistanceFunction_Euclidean);
     noise.SetCellularJitter(0.25);
 
-    prepare_matrix(mat, noise);
-    print_matrix(mat);
+    auto [start, end] = prepare_matrix(mat, noise);
+    prtype_matrix(mat);
+    find_shortest_path(mat, start, end);
 
 
     return 0;
